f1c8050c3626848d0ef95787138b3ae3faea6efb
Nikolaos <nzoump@gmail.com>
2021-11-16 12:00:17 +0200

    First Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Employees from "./components/Employees";
+import EmployeeForm from "./components/EmployeeForm";
+import NotFound from "./components/NotFound";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<ToastContainer />
+			<Switch>
+				<Route path="/employees/:id" component={EmployeeForm} />
+				<Route path="/employees" exact component={Employees} />
+				<Route path="not-found" component={NotFound} />
+				<Redirect from="/" to="/employees" />
+				<Redirect to="/not-found" />
+			</Switch>
+		</React.Fragment>
+	);
 }
 
 export default App;
